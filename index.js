56c27269312f01e81ef8cf7f5b4fb3e335b01ee5
Reqyan <reqyan.1103@gmail.com>
2023-02-20 12:16:40 +0700

    adding mongoDB in docker

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("mongodb://admin:admin@mongo:27017/?authSource=admin")
+.then(() => console.log("Succesfully connect to DB"))
+.catch((e) => console.log(e));
 
 app.get("/", (req, res)=>{
     res.send("<h2>hi There!!! someone</h2>");
